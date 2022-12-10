431c65aa2579d0e56f63120a88e62b2574331576
Kiny <89192734912nik@gmail.com>
2022-12-11 00:36:17 +0300

    development

---
+++ project2 GuessTheFlag/project2 GuessTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2 GuessTheFlag
+//
+//  Created by nikita on 11.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
